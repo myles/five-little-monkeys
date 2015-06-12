4d16d822cba920370c11dd5984327fd2f75589e8
Myles Braithwaite <me@mylesbraithwaite.com>
2015-06-12 10:37:26 -0400

    Added the last little bit.

--- five-little-monkeys.rb
+++ five-little-monkeys.rb
@@ -9,8 +9,16 @@ last_line = 'No more monkeys jumping on the bed'
 
 last_last_line = 'Put those monkeys right to bed'
 
+numbers = {
+  1 => 'One',
+  2 => 'Two',
+  3 => 'Three',
+  4 => 'Four',
+  5 => 'Five'
+}
+
 (1..5).reverse_each do |x|
   last_line = last_last_line if x == 1
 
-  puts lyrics % { number: x, last_line: last_line }
+  puts lyrics % { number: numbers[x], last_line: last_line }
 end
