89bb1215cbb9d1c60e68876d40c0c3efe52f0f66
Myles Braithwaite <me@mylesbraithwaite.com>
2015-12-01 14:55:02 -0500

    Upgrading the code to be compatable with Swift 2.

--- five-little-monkeys.swift
+++ five-little-monkeys.swift
@@ -1,4 +1,4 @@
-var numberOfMonkeys = [
+let numberOfMonkeys: [Int:String] = [
 	1: "One",
 	2: "Two",
 	3: "Three",
@@ -6,23 +6,23 @@ var numberOfMonkeys = [
 	5: "Five"
 ]
 
-let sortedNumberOfMonkeys = Array(numberOfMonkeys.keys).sorted(>)
+let sortedNumberOfMonkeys = numberOfMonkeys.sort {$0.0 < $1.0}
 
-for (key, value) in enumerate(sortedNumberOfMonkeys) {
+for (index, (key, value)) in sortedNumberOfMonkeys.enumerate() {
 	if key == 1 {
-		println("\(value) little moneky jumping on the bed")
-		println("They fell off and bumped their head")
+		print("\(value) little moneky jumping on the bed")
+		print("They fell off and bumped their head")
 	} else {
-		println("\(value) little monekys jumping on the bed")
-		println("One fell off and bumped their head")
+		print("\(value) little monekys jumping on the bed")
+		print("One fell off and bumped their head")
 	}
 	
-	println("Mama called the doctor,")
-	println("And the doctor said")
+	print("Mama called the doctor,")
+	print("And the doctor said")
 	
 	if key == 1 {
-		println("Put those monkeys right to bed\n")
+		print("Put those monkeys right to bed\n")
 	} else {
-		println("No more monkeys jumping on the bed\n")
+		print("No more monkeys jumping on the bed\n")
 	}
 }
