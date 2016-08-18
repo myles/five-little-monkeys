e129aa101badc0c5ce95b47829837b32d610f7ec
Myles Braithwaite <myles@users.noreply.github.com>
2016-08-17 23:23:35 -0400

    Update five-little-monkeys.js

--- five-little-monkeys.js
+++ five-little-monkeys.js
@@ -8,7 +8,7 @@
 var numberOfMonkeys = ["Five", "Four", "Three", "Two", "One"];
 
 numberOfMonkeys.forEach(function (value) {
-    if (value == "One") {
+    if (value === "One") {
         console.log(value + " little monkey jumping on the bed");
         console.log("They fell off and bumped their head");
     } else {
@@ -19,7 +19,7 @@ numberOfMonkeys.forEach(function (value) {
     console.log("Mama called the doctor,");
     console.log("And the doctor said");
 
-    if (value == "One") {
+    if (value === "One") {
         console.log("Put those monkeys right to bed\n");
     } else {
         console.log("No more monkeys jumping on the bed\n");
